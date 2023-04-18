fbbd025e232cedd9a73828c4a8031f49eb0309a4
Helder Oliveira <helder@gmail.com>
2023-04-17 21:02:00 -0300

    Aula 2

---
+++ aula2.py
@@ -0,0 +1,15 @@
+nome = input ("Informe seu nome: ")
+idade = input ("Informe sua idade: ")
+
+#Isso é um comentário de uma linha
+"""
+Isso é um comentário de 
+várias 
+linhas 
+
+"""
+print ("Olá {} Sua idade é  {}".format(nome, idade))
+print (f"Olá {nome} Sua idade é  {idade}")
+
+
+
