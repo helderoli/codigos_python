231de170ca6b8bce90aaf5201faa1d0a6cf69d31
Helder Oliveira <helder@gmail.com>
2023-04-18 21:12:05 -0300

    Criado Aula 3 python

---
+++ aula3.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+nota3 = float(input("Informe a nota 3: "))
+
+media = (nota1 + nota2 + nota3) / 3
+print(f"A média final é {media}\n")
+
+
