b41d7a081194de3425550e477f8401a144a34dae
Helder Oliveira <helder@gmail.com>
2023-04-20 19:09:27 -0300

    atividade 4 concluida

--- atividade sequencial senai/atividade4.py
+++ atividade sequencial senai/atividade4.py
@@ -5,7 +5,12 @@ garrafa = int(input("Informe a quantidade de garrafas de 600ml: ")) #garrafa 600
 litro = int(input("Informe a quantidade de litro de 2000ml: ")) #litro de 2000ml
 
 print("A quantidade de refrigerantes lata de 350ml são:",lata, )
-print("A quantidade de refrigerantes lata de 350ml são:",lata, )
+print("A quantidade de refrigerantes garrafa de 350ml são:",garrafa, )
+print("A quantidade de refrigerantes litro de 350ml são:", litro, )
+
+soma = (lata*0.35) + (garrafa*0.6) + (litro*2)
+
+print ("A quantidade total de litros é de ", soma, "Litros")
 
 
 
