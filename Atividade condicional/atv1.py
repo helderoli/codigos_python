b59549e7d40f6112419e607e9208b3d638589033
Helder Oliveira <helder@gmail.com>
2023-04-20 21:09:14 -0300

    aula 1 atividade py

---
+++ Atividade condicional/atv1.py
@@ -0,0 +1,7 @@
+valor = int(input("Informe um valor qualquer: "))
+
+if valor >=100 and valor<=200:
+    print(f"O valor {valor} está entre 100 e 200\n")
+else:
+    print(f"O valor {valor} não está entre 100 e 200\n")
+    
